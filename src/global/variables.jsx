f4f3b26b41f2f5132831c057c1090fcdac3c8f44
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-07-07 20:51:51 -0300

    Upload de variáveis

---
+++ src/global/variables.jsx
@@ -0,0 +1,23 @@
+class Variaveis{
+    static verificarHorario = (horario) => {
+        horario = horario.toString();
+        horario = horario.split('');
+        if(horario[1] === undefined) {
+            horario.unshift('0');
+            return horario.toString().replace(',', '');
+        } else {
+            return horario.toString().replace(',', '');
+        };
+
+        /* if(this.horaAtual[4] == undefined) {
+            this.horaAtual = this.horaAtual.split('');
+            this.horaAtual.splice(3, 0, '0');
+        }; */
+    };
+    
+    static data = new Date;
+    static horaAtual = `${this.verificarHorario(this.data.getHours())}:${this.verificarHorario(this.data.getMinutes())}`;
+
+};
+
+export default Variaveis;
