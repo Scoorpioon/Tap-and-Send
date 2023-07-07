799879a38af5c26422b6f433929e30b5d4da6d7e
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-07-07 20:55:48 -0300

    Adição de diversas informações
    Adicionado nome, foto, última mensagem e botão de edição de contato

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,10 +1,24 @@
 import React, {useRef, useState, lazy} from 'react';
 import '../styles/contactBar.css';
+import dadosMensagem from '../global/data';
 
-const Contato = ({nome, texto}) => {
+const Contato = ({nome, texto, id}) => {
+    const selecionarContato = () => {
+        console.log('ID do contato selecionado: ' + id)
+    }
+
     return(
-        <div className="contact">
-            <p>*Contato*</p>
+        <div className="_contato" onClick={selecionarContato}>
+            <div className="_info-contato">
+                <span className="foto-contato">Foto</span>
+                <div className="textos-contato">
+                    <span className="nome-contato">{nome}</span>
+                    <span className="mensagem-contato">{texto}</span>
+                </div>
+            </div>
+            <div className="_editar-contato">
+                <span>E.</span>
+            </div>
         </div>
     );
 };
