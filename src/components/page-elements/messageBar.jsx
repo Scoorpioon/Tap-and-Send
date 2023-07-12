bc3b1390fb6cdc65f3e3861559fcf2403ffd6395
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-07-12 19:22:06 -0300

    Atualizações de mensagem vazia
    Atualizando as pequenas alterações de tentativa de enviar mensagem vazia.

--- src/components/page-elements/messageBar.jsx
+++ src/components/page-elements/messageBar.jsx
@@ -1,21 +1,30 @@
 import React, {useRef, useState, useEffect, useReducer} from 'react';
 import Mensagem from '../message';
-import '../../styles/messageContainer.css';
+import '../../styles/messageBar.css';
 import dadosMensagem from '../../global/data';
 import Variaveis from '../../global/variables';
 
 const BarraDeEnvio = (props) => {
     const textoDigitado = useRef();
-    const [mensagem, setarMensagem] = useState()
+    const [mensagem, setarMensagem] = useState();
+    const [mensagemVazia, ticarMensagemVazia] = useState();
+    const [placeholder, mudarPlaceholder] = useState('Mensagem a ser enviada...');
 
     const enviarMensagem = () => {
         const texto = textoDigitado.current.value;
         
         if(!texto) {
-            console.log('Mensagem vazia não pode ser enviada...!');
+            console.log('Mensagem vazia bloqueada.');
+
+            ticarMensagemVazia('animacaoErro');
+            mudarPlaceholder('Mensagem vazia não pode ser enviada...!');
+
             return false;
         } else {
             setarMensagem(texto);
+
+            ticarMensagemVazia();
+            mudarPlaceholder('Mensagem a ser enviada...')
         };
 
         props.onClick(mensagem);
@@ -28,12 +37,12 @@ const BarraDeEnvio = (props) => {
 
     return(
         <div className="barraDeEnvio">
-            <input type="text" ref={textoDigitado} placeholder="Mensagem a ser enviada..." onKeyDown={(evento) => {
+            <input type="text" ref={textoDigitado} placeholder={placeholder} onKeyDown={(evento) => {
                 if(evento.key === 'Enter') {
                     enviarMensagem();
                 };
             }} />
-            <button onClick={enviarMensagem}>{'>'}</button>
+            <button onClick={enviarMensagem} className={`botaoDeEnvio ${mensagemVazia}`}>{'>'}</button>
         </div>
     );
 };
