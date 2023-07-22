c742154bf9ed1cdbf9f9ac6cfe7d71a2f5363d1f
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-07-22 13:58:31 -0300

    Reformulação de dados
    Context API

--- src/components/page-elements/messageContainer.jsx
+++ src/components/page-elements/messageContainer.jsx
@@ -1,45 +1,38 @@
-import React, {useRef, useState, useEffect, useReducer} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {DadosDeMensagens} from '../../global/mainData';
 import Mensagem from '../message';
-import '../../styles/messageContainer.css';
-import dadosMensagem from '../../global/data';
 import Variaveis from '../../global/variables';
 import BarraDeEnvio from './messageBar';
+import '../../styles/messageContainer.css';
 
-const CaixaMensagens = ({}) => {
+const CaixaMensagens = () => {
     const [mensagem, setarMensagem] = useState(); // Mensagem a ser enviada para a caixa de mensagens
-    let idAtual = Variaveis.idAtual;
-    let contatoAtual = dadosMensagem.contatos[idAtual];
+    const {mensagensAPI, enviarTextoAPI} = useContext(DadosDeMensagens); // Import do Context API (variáveis globais)
+
+    useEffect(() => {
+       console.log('Context API: ', mensagensAPI)
+    }, [mensagensAPI]);
 
     const receberMensagem = (textoMensagem) => {
         setarMensagem(textoMensagem)
-    }
+    };
 
     useEffect(() => {
-        enviarTexto((mensagensAnteriores) => [...mensagensAnteriores, {
+        enviarTextoAPI((msgsAnt) => [...msgsAnt, {
             textoDaMensagem: mensagem,
             autor: 'author',
             horario: Variaveis.horaAtual
         }]);
 
-        contatoAtual = mensagens;
-
     }, [mensagem]); // useEffect necessário, pois sem ele a mensagem não é atualizada instantaneamente
-
-    const [mensagens, enviarTexto] = useState(contatoAtual); // Todas as mensagens armazenadas que já foram enviadas
-
-    useEffect(() => {
-        enviarTexto(contatoAtual);
-    }, [contatoAtual]);
 
     return(
         <>
             <div className="_messageContainer">
-                <div className="headerMessageContainer">
-                    
-                </div>
-
                 <div className="messages">
-                    {mensagens.map((msg) => {return <Mensagem key={Math.random()} texto={msg.textoDaMensagem} tipo={msg.autor} hora={msg.horario} />})}
+                    {mensagensAPI.map((msg) => {return <Mensagem key={Math.random()} texto={msg.textoDaMensagem} tipo={msg.autor} hora={msg.horario} />})}
+                    {/* <Mensagem texto={mensagem} tipo="author" />
+                    <Mensagem texto="Mensagem da outra pessoa!" tipo="receiver" /> */}
                 </div>
                 <BarraDeEnvio onClick={receberMensagem} />
             </div>
