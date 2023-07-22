2461041e047795551bcb102b74f8402848350d04
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-07-22 13:56:34 -0300

    "Banco de dados" provisório
    Um arquivo separado feito para guardar as informações de mensagens e contatos temporariamente, enquanto o back-end ainda não é implantado no projeto

---
+++ src/global/provisionalDB.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Variaveis from "./variables";
+
+class dadosMensagem{
+    static mensagens = [
+        {
+            textoDaMensagem: 'Eu sou a sua mensagem!',
+            autor: 'author',
+            hora: Variaveis.horaAtual
+        },
+        {
+            textoDaMensagem: 'Eu sou a mensagem do seu amigo!',
+            autor: 'receiver',
+            hora: Variaveis.horaAtual
+        }
+    ]; /* =======================CÓDIGO ANTERIOR DE CAIXA DE MENSAGEM ÚNICA======================= */
+
+   static contatos = {
+    Contato0: [
+    {
+        textoDaMensagem: 'Bom dia! Tudo bem com você?',
+        autor: 'receiver',
+        horario: Variaveis.horaAtual
+    },
+    {
+        textoDaMensagem: 'Estamos conversando através de uma aplicação web em React desenvolvida por Gabriel Alexandre!',
+        autor: 'receiver',
+        horario: Variaveis.horaAtual
+    },
+    {
+        textoDaMensagem: 'Que incrível!',
+        autor: 'author',
+        horario: Variaveis.horaAtual
+    }
+],
+    Contato1: [{
+        textoDaMensagem: 'Este é um exemplo de alteração de conversas.',
+        autor: 'receiver',
+        horario: Variaveis.horaAtual
+    },
+    {
+        textoDaMensagem: 'Você pode criar um novo contato e uma nova caixa de conversas será criada!',
+        autor: 'receiver',
+        horario: Variaveis.horaAtual
+    },
+    {
+        textoDaMensagem: 'Entendi...! Obrigado pela informação, meu amigo.',
+        autor: 'author',
+        horario: Variaveis.horaAtual
+    }]
+    /*
+    Quando um usuário criar um novo contato, um novo parâmetro será adicionado aqui neste formato, seguindo sempre de uma chave que contém uma lista de objetos. Nesta lista, sempre serão armazenados o texto, o autor e o horario. Conforme a evolução do projeto, o 'texto da mensagem' se tornará 'conteúdo da mensagem', pois o usuário também poderá enviar imagens, áudios, etc...
+
+    Exemplo:
+    
+    Contato2: [{
+        textoDaMensagem: 'Mensagem aqui...',
+        autor: 'Autor da mensagem',
+        horario: Hora que será armazenada na mensagem
+    }
+    */
+   }
+};
+
+export default dadosMensagem;
