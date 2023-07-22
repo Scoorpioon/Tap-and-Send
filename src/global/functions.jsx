c6fa34da152bfd246bd77a4e81e61ad0e3e315df
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-07-22 13:53:22 -0300

    Arquivo desnecessário

--- src/global/functions.jsx
+++
@@ -1,9 +0,0 @@
-class Funcoes{
-    static atalhoEnviar = (event) => {
-            if(event.keyCode === 'Enter') {
-                return true;
-            };
-    };
-};
-
-export default Funcoes
