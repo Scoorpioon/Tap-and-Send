9cd6e9328917df3679d1b91d379190c9bdce4635
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-07-22 13:57:33 -0300

    Implantação de Context API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import {useState} from 'react';
 import './styles/App.css';
-import {Button, Alert, Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import CaixaMensagens from './components/page-elements/messageContainer';
 import BarraContatos from './components/page-elements/contactBar';
-import {lazy} from 'react';
+import dadosMensagem from './global/provisionalDB';
+import {DadosDeMensagens} from './global/mainData'
+/* import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' */
 
 function App() {
+  const [mensagensAPI, enviarTextoAPI] = useState(dadosMensagem.contatos['Contato0']);
+  const [contatos, setarContato] = useState();
+
   return (
     <main>
-      <BarraContatos />
-      <CaixaMensagens />
+      <DadosDeMensagens.Provider value={{mensagensAPI, enviarTextoAPI}}>
+        <BarraContatos />
+        <CaixaMensagens />
+      </DadosDeMensagens.Provider>
     </main>
   );
 };
