dbbf3f798c9f2150268d7a56c2bf6a7f728b3c36
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-08-15 15:57:00 -0300

    Atualização geral

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,25 +1,32 @@
-import {useState, useContext} from 'react';
+import {useState, useRef} from 'react';
+import {useDispatch} from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faMagnifyingGlass, faXmark} from '@fortawesome/free-solid-svg-icons';
-import { DadosDoUsuario } from '../global/userData';
 import '../styles/searchBar.css';
 
-const BarraDePesquisa = ({Placeholder, tipo, botao}) => {
+const BarraDePesquisa = ({Placeholder, tipo, identificacao}) => {
     const [barraVisivel, alterarVisibilidade] = useState();
-    const {pesquisa, atualizarPesquisa} = useContext(DadosDoUsuario);
+    const mensagemBuscada = useRef();
+    const dispatch = useDispatch();
 
     const aparecerBarra = () => {
         return barraVisivel ? alterarVisibilidade(false) : alterarVisibilidade(true);
-    }
+    };
 
-    const dadosPesquisa = (evento) => {
-        atualizarPesquisa(evento.target.value);
-    }
+    const dadosPesquisa = () => {
+        dispatch({
+            type: 'pesquisa/atualizar',
+            nomeBarra: mensagemBuscada.current.id,
+            dadosPesquisa: mensagemBuscada.current.value
+        });
+    };
 
     return(
         <>
-            {tipo === 'Barra' || barraVisivel ? <input type="text" onChange={dadosPesquisa} className="searchBar" placeholder={Placeholder} onAnimationStart={(e) => {console.log('Animação!')}} /> : null}
-            {tipo === 'Click' ? <button className={`lupa`} onClick={aparecerBarra}>{!barraVisivel ? <FontAwesomeIcon icon={faMagnifyingGlass} /> : <FontAwesomeIcon icon={faXmark} />}</button> : null}
+            {tipo === 'Barra' || barraVisivel ? <input type="text" onChange={dadosPesquisa} id={identificacao} className="searchBar" 
+            placeholder={Placeholder} ref={mensagemBuscada} /> : null}
+            {tipo === 'Click' ? <button className={`lupa`} onClick={aparecerBarra}>{!barraVisivel ? <FontAwesomeIcon icon={faMagnifyingGlass} /> 
+            : <FontAwesomeIcon icon={faXmark} />}</button> : null}
         </>
     );
 };
