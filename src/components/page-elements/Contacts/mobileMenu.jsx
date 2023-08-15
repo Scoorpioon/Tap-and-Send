b5634d797095f7fd470c7b0590ed514d8738a364
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-08-15 15:51:31 -0300

    Implementação menu mobile

---
+++ src/components/page-elements/Contacts/mobileMenu.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../../styles/mobileMenu.css';
+
+const MenuMobile = () => {
+    const opcaoClicada = () => {
+        console.log('Opção clicada!');
+    }
+
+    return(
+        <nav className="_mobileNavigation">
+            <ul>
+                <li><button onClick={opcaoClicada} className="opcaoMenu">Contatos</button></li>
+                <li><button onClick={opcaoClicada} className="opcaoMenu">Chat</button></li>
+                <li><button onClick={opcaoClicada} className="opcaoMenu">Perfil</button></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default MenuMobile;
