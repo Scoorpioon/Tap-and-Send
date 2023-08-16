a5ba26747f51d448c82c8e2615b3d770f10b1051
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-08-16 10:30:47 -0300

    Correção de Reducers

--- src/global/redux/root-reducer.js
+++ src/global/redux/root-reducer.js
@@ -1,6 +1,9 @@
 import {combineReducers} from 'redux';
 import userReducer from './Reducers/reducer';
+import msgReducer from './Reducers/msgReducer';
+import contactReducer from './Reducers/contactReducer';
+import searchReducer from './Reducers/searchReducer';
 
-const rootReducer = combineReducers({userReducer});
+const rootReducer = combineReducers({userReducer, msgReducer, contactReducer, searchReducer});
 
 export default rootReducer;
