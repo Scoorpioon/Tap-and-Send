ca03511ab554c1ebd3765f5cd144b4955ee6e392
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-08-18 15:02:26 -0300

    Integrando dados gerais e novo action
    Foram mesclados todos os dados possíveis dos contatos no reducer de contato. Também foi adicionado o action de enviar mensagem diretamente neste pedaço do código, para resolução de um bug. Este esquema pode ser alterado futuramente.

--- src/global/redux/Reducers/contactReducer.js
+++ src/global/redux/Reducers/contactReducer.js
@@ -1,3 +1,5 @@
+import Variaveis from "../../TemporaryClasses/variables";
+
 const contatoInicial = {
     ContatoAtual: 'Contato0',
     idAtual: 0, 
@@ -6,14 +8,48 @@ const contatoInicial = {
         id: 0,
         numero: 40028922,
         nome: 'Matheus Godoy',
-        mensagem: 'Eai mano, assistiu o filme que te falei?',
+        mensagemAtual: 'Eai mano, assistiu o filme que te falei?',
+        mensagens: [
+          {
+            textoDaMensagem: 'Bom dia! Tudo bem com você?',
+            autor: 'receiver',
+            horario: Variaveis.horaAtual
+          },
+          {
+            textoDaMensagem: 'Estamos conversando através de uma aplicação web em React desenvolvida por Gabriel Alexandre!',
+            autor: 'receiver',
+            horario: Variaveis.horaAtual
+          },
+          {
+            textoDaMensagem: 'Que incrível!',
+            autor: 'author',
+            horario: Variaveis.horaAtual
+          }
+        ]
         /* foto: '***' */
       }, 
       {
         id: 1,
         numero: 40028922,
         nome: 'Alef Kwanzas',
-        mensagem: 'Foi tipo isso'
+        mensagemAtual: 'Foi tipo isso',
+        mensagens: [
+          {
+            textoDaMensagem: 'Este é um exemplo de alteração de conversas.',
+            autor: 'receiver',
+            horario: Variaveis.horaAtual
+          },
+          {
+            textoDaMensagem: 'Você pode criar um novo contato e uma nova caixa de conversas será criada!',
+            autor: 'receiver',
+            horario: Variaveis.horaAtual
+          },
+          {
+            textoDaMensagem: 'Entendi...! Obrigado pela informação, meu amigo.',
+            autor: 'author',
+            horario: Variaveis.horaAtual
+          }
+        ]
       }
     ]
 };
@@ -29,9 +65,21 @@ const contactReducer = (state = contatoInicial, action) => {
             break;
 
         case 'contato/adicionarTeste':
-            console.log(state);
             return {...state, ListaDeContatos: action.novoContato};
             break;
+
+        case 'contato/enviarmensagem':
+          if(!action.mensagensAtualizadas.textoDaMensagem) {
+            return {...state};
+          };
+
+          const State = {...state}
+          const novaLista = [...State.ListaDeContatos[action.idAtual].mensagens];
+
+          novaLista.push(action.mensagensAtualizadas);
+
+          State.ListaDeContatos[action.idAtual].mensagens = novaLista;
+          return State;
     };
 
     return state;
